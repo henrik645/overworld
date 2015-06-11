975cc16eb215a8fddbfc9d283c091fd6594278c5
Henrik Vester <henrik645@gmail.com>
2015-06-11 20:50:44 +0200

    Fix issue where screen weren't refreshed
    The screen wasn't previously refreshed when the 'update_screen' method was called.

--- screen.py
+++ screen.py
@@ -28,6 +28,7 @@ def update_screen(screen, level, player=None):
     
     if player is not None: #A player needs to be plotted
         screen.addstr(player.y_pos, player.x_pos, tile.tile_symbols[tile.TileType.player])
+    screen.refresh()
     
 def update_status_line(screen, status):
     screen.move(0, 0)
