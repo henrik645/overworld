63c466972fbc204620b3ee52f1739464c5a3729c
Henrik Vester <henrik645@gmail.com>
2015-06-11 19:50:33 +0200

    Add code for status window
    Add code for utilisation of 'status.py' which contains functions for
    printing status lines.

--- main.py
+++ main.py
@@ -8,31 +8,35 @@ import curses
 import screen
 import world
 import player
+import status
+
+level_width = 80
+level_height = 20
 
 stdscr = screen.init_screen()
+status_screen = status.init_screen(level_width, level_height)
 
-screen_width = 40
-screen_height = 20
-
-level = world.Level(screen_width, screen_height)
+level = world.Level(level_width, level_height)
 player = player.Player(10, 10, 5, 5)
 
 while True: #Main loop
     screen.update_screen(stdscr, level, player)
+    screen.update_player_status(status_screen, player)
     key = stdscr.getch()
     if key == curses.KEY_UP:
         if player.y_pos > 0:
             player.y_pos -= 1
     elif key == curses.KEY_DOWN:
-        if player.y_pos < screen_height - 1: #Minus once since coordinates start at 0 instead of 1
+        if player.y_pos < level_height - 1: #Minus once since coordinates start at 0 instead of 1
             player.y_pos += 1
     elif key == curses.KEY_LEFT:
         if player.x_pos > 0:
             player.x_pos -= 1
     elif key == curses.KEY_RIGHT:
-        if player.x_pos < screen_width - 1: #Minus once since coordinates start at 0 instead of 1
+        if player.x_pos < level_width - 1: #Minus once since coordinates start at 0 instead of 1
             player.x_pos += 1
     elif key == ord('q'):
         break
 
 screen.end(stdscr)
+screen.end(status_screen)
