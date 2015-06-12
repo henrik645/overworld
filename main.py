6842a2c7004ae4450562664864c6e28b8504e227
Henrik Vester <henrik645@gmail.com>
2015-06-12 12:00:08 +0200

    Savefile delete on death
    Now the savefile will delete itself when the player dies.

--- main.py
+++ main.py
@@ -25,9 +25,11 @@ if os.path.isfile(config['save_file']):
     save_game = save.load_game(config['save_file'])
     level = save_game.world_save
     character = save_game.player_save
+    save_exists = True
 else:
     level = world.Level(level_width, level_height)
     character = player.Player(10, 10, 5, 5)
+    save_exists = False
 
 while True: #Main loop
     screen.update_screen(stdscr, level, character)
@@ -35,6 +37,8 @@ while True: #Main loop
     character.check()
     if not character.alive:
         status.status(status_screen, "You die...")
+        if save_exists:
+            os.remove(config['save_file'])
         break
         
     key = stdscr.getch()
